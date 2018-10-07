f21207192de6d0b4ab662406db7e51db51be3849
vajid.kagdi@sjsu.edu <ec2-user@ip-172-31-10-75.ec2.internal>
2018-10-07 03:15:09 +0000

    DB name changed

--- app.js
+++ app.js
@@ -54,8 +54,10 @@ var db_pwd  = "cmpe281" ;
 var db_name = "test" ;
 */
 
-var db_host = (process.env.mongodb_host || "localhost" ) ;
-var db_port = (process.env.mongodb_port || "27017" ) ;
+var db_host = (process.env.mongodb_host || "ds225253.mlab.com" ) ;
+//var db_host = (process.env.mongodb_host || "localhost" ) ;
+//var db_port = (process.env.mongodb_port || "27017" ) ;
+var db_port = (process.env.mongodb_port || "25253" ) ;
 var db_user = (process.env.mongodb_user || "cmpe281" ) ;
 var db_pwd  = (process.env.mongodb_pwd  || "cmpe281" ) ;
 var db_name = (process.env.mongodb_name || "heroku_g45rfffq" ) ;
